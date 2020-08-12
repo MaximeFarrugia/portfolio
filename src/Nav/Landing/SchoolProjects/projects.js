03ff2d0335ac95fac2910f942afa90e91b203569
Maxime Farrugia <farrugia.maxime@gmail.com>
2020-08-12 19:54:32 +0200

    feat: added zappy school project

--- src/Nav/Landing/SchoolProjects/projects.js
+++ src/Nav/Landing/SchoolProjects/projects.js
@@ -7,6 +7,12 @@ const projects = [
     tags: ['C', 'Assembly', 'Game', 'Memory'],
   },
   {
+    project: 'Zappy',
+    desc: 'Compilateur / arène de combat de champions informatique.',
+    link: 'https://github.com/MaximeFarrugia/PSU_zappy_2019',
+    tags: ['C', 'Python', 'Unity', 'IA', 'Game', 'Network'],
+  },
+  {
     project: 'Hackaton Jaguar Network',
     desc: 'Trouver des solutions pour la SmartMétropole.',
     link: 'https://community.jaguar-network.com/sprintoneweek-epitech-medinsoft-jaguar-network-3-projets-primes/',
