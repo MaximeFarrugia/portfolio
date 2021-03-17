ceb6dda35e1e002afcf193fa7822c6e1ed2c0bb6
Maxime FARRUGIA <farrugia.maxime@gmail.com>
2021-03-17 16:23:24 +0100

    fix: removed useless packages

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import 'react-intersection-observer/test-utils'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
