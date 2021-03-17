d11b92a3ab28412bd649f95cd56b34b931ed6005
Maxime FARRUGIA <farrugia.maxime@gmail.com>
2021-03-17 12:43:46 +0100

    feat: implementing styled-components on tag

--- src/Common/Components/Tag/index.js
+++ src/Common/Components/Tag/index.js
@@ -1,28 +1,21 @@
-import React, { useContext } from 'react'
+import React from 'react'
+import styled from 'styled-components'
 import { string } from 'prop-types'
 
-import { Wrapper, Dark, Light } from './Tag.module.css'
-
-import classNames from '../../Helpers/classNames'
-import { Context } from '../../../App'
-
-const Tag = ({ className, name }) => {
-  const { darkTheme } = useContext(Context)
-
-  return (
-    <div className={classNames([className, Wrapper, darkTheme ? Dark : Light])}>
-      {name}
-    </div>
-  )
-}
+const Tag = ({ name }) => <Wrapper>{name}</Wrapper>
 
 Tag.propTypes = {
-  className: string,
   name: string.isRequired,
 }
 
-Tag.defaultProps = {
-  className: '',
-}
+const Wrapper = styled.div`
+  font-size: 14px;
+  border: 1px solid;
+  border-radius: 5px;
+  width: fit-content;
+  padding: 2px 10px;
+  color: ${props => props.theme.secondary};
+  border-color: ${props => props.theme.secondary};
+`
 
 export default Tag
